fa5a8f874a5f89cf90603e9a4d24c6f2fa6321a5
deion <db101097@gmail.com>
2019-06-05 10:44:35 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
+import React , {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class FormEdit extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			zip:this.props.zip,
+			results:this.props.results
+		}	
+
+		this.Submit=this.Submit.bind(this);
+		this.modZip=this.modZip.bind(this);
+	}
+
+
+	Submit(event){
+		event.preventDefault();
+		fetch('http://ctp-zip-api.herokuapp.com/zip/'+this.state.zip)
+			.then(function(response){
+				return response.json();
+			})
+			.then(function(json){
+				console.log(JSON.stringify(json))
+			})
+			.catch(function(err) {
+				console.log(err);
+			});
+	}
+
+
+	modZip(event){
+		this.setState({zip:event.target.value})
+		console.log(this.state.zip);
+	}
+	
+
+    render() {
+				return(<form>
+				  Zip Code:
+				  <input type="text" onChange={this.modZip}></input>
+				  <button onClick={this.Submit}>Submit</button>
+				</form> )
+	}
+}
+
+
+class App extends Component {
+	render() {
+		return <div className="container">
+		<FormEdit/>
+		</div>
+	}
 }
 
 export default App;
